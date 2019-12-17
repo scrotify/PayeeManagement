6c3b31b9b0cbea819d3a3bdaa59b2aba277097f2
scrotifyteam <scrotifyteam@gmail.com>
2019-12-17 13:58:15 +0530

    added logs
    added logs

--- src/main/java/com/scrotifybanking/payeemanagement/service/CustomerServiceImpl.java
+++ src/main/java/com/scrotifybanking/payeemanagement/service/CustomerServiceImpl.java
@@ -7,6 +7,8 @@ import com.scrotifybanking.payeemanagement.entity.Customer;
 import com.scrotifybanking.payeemanagement.exception.CustomException;
 import com.scrotifybanking.payeemanagement.repository.CustomerRepository;
 import com.scrotifybanking.payeemanagement.web.ScrotifyConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,11 +17,13 @@ import java.util.Optional;
 
 /**
  * The type Customer service.
+ *
  * @author Mahendran
  */
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+    public static final Logger logger = LoggerFactory.getLogger(CustomerServiceImpl.class);
     /**
      * The Customer repository.
      */
@@ -31,12 +35,14 @@ public class CustomerServiceImpl implements CustomerService {
      */
     @Override
     public Optional<LoginResponseDto> loginCustomer(LoginRequestDto loginRequestDto) throws CustomException {
+        logger.info("Login customer ");
         LoginResponseDto loginResponseDto = new LoginResponseDto();
         Optional<LoginResponseDto> loginResponseDtoOptional = Optional.empty();
         Long customerId = loginRequestDto.getId();
         String password = loginRequestDto.getPassword();
         Optional<Customer> customer = customerRepository.findByCustomerId(customerId);
         if (customer.isPresent()) {
+            logger.info("Login customer present");
             loginResponseDtoOptional = customer
                     .filter(cust -> cust.getCustomerId().equals(customerId))
                     .filter(cust -> cust.getCustomerPassword().equals(password))
@@ -47,6 +53,7 @@ public class CustomerServiceImpl implements CustomerService {
             loginResponseDto.setStatusCode(ScrotifyConstant.SUCCESS_CODE);
             loginResponseDto.setMessage(ScrotifyConstant.SUCCESS_MESSAGE);
         } else {
+            logger.info("Login customer not present");
             loginResponseDto.setStatusCode(ScrotifyConstant.FAILURE_CODE);
             loginResponseDto.setMessage(ScrotifyConstant.INVALID_MESSAGE);
         }
